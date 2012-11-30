5230376e189d68be01ec19ff40df68dcf51c28bc
Bruno Perel <brunoperel@gmail.com>
2012-12-01 00:06:24 +0100

    Remove obsolete file

--- parser/requete.sql
+++
@@ -1,5 +0,0 @@
-SELECT co.numeroContrat AS NUMERO, co.origineContrat, co2.aaa, e.codeEntite
-FROM Contrat co, Contrat co2, EtabPost e
-WHERE co.typeContrat='C' AND co.numeroContrat > 0 
-AND co.codeEntite = e.codeEntite
-ORDER BY co.dateContrat;
