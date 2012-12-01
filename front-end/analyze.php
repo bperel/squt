61a48fc247f7fce1ebea48848cb05896302f02bf
Bruno Perel <brunoperel@gmail.com>
2012-12-01 21:26:45 +0100

    Typos and variable names

--- front-end/analyze.php
+++ front-end/analyze.php
@@ -12,17 +12,17 @@ else $os = 'UnKnown';
 $query = str_replace('"','\"',str_replace("\n"," ",$_GET['query']));
 $path_to_perl = ($os == 'Windows' ? $PATH_TO_CYGWIN.'/bin/' : '');
 
-if (!file_exists($ERROR_OUTPUT)) {
-	echo 'Error - The file '.$ERROR_OUTPUT.' has not been found in the /front-end directory.';
+if (!file_exists($ERROR_OUTPUT_FILE)) {
+	echo 'Error - The file '.$ERROR_OUTPUT_FILE.' has not been found in the /front-end directory.';
 	exit(0);
 }
 
 ob_start();
 $command = '"'.$path_to_perl.'perl" "'.$PATH_SQUT.'squt/parser/myparse_to_squt.pl" "'.$query.'" 2> '.$ERROR_OUTPUT;
 $result = shell_exec($command);
-$error_output = file_get_contents($ERROR_OUTPUT);
+$error_output = file_get_contents($ERROR_OUTPUT_FILE);
 if (!empty($error_output)) {
-	echo json_encode(array('Error'=>file_get_contents($ERROR_OUTPUT)));
+	echo json_encode(array('Error'=>file_get_contents($ERROR_OUTPUT_FILE)));
 }
 else {
 	echo $result;
--- front-end/config.php
+++ front-end/config.php
@@ -1,5 +1,5 @@
 <?php
 $PATH_TO_CYGWIN="C:/cygwin"; // Ignore this line if you're not using Cygwin
-$PATH_SQUT="/usr/src/"; // Ignore this line if you're not using Cygwin
-$ERROR_OUTPUT="error_output.log";
+$PATH_SQUT="/usr/src/";
+$ERROR_OUTPUT_FILE="error_output.log";
 ?>
