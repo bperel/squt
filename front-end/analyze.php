0017b4389fa7ef95eadb48d6d3d43d76e8df82ca
Bruno Perel <brunoperel@gmail.com>
2013-02-04 23:55:45 +0100

    Graceful degradation mode. Fixes #8

--- front-end/analyze.php
+++ front-end/analyze.php
@@ -9,37 +9,42 @@ elseif(preg_match('/Win/',$agent)) $os = 'Windows';
 elseif(preg_match('/Mac/',$agent)) $os = 'Mac';
 else $os = 'UnKnown';
 
-$query = str_replace('"','\"',str_replace("\n"," ",$_GET['query']));
-$is_debug = isset($_GET['debug']) && $_GET['debug'] == 1;
-$path_to_perl = ($os == 'Windows' ? $PATH_TO_CYGWIN.'/bin/' : '');
-
-if (!file_exists($ERROR_OUTPUT_FILE)) {
-	echo 'Error - The file '.$ERROR_OUTPUT_FILE.' has not been found in the /front-end directory.';
-	exit(0);
-}
-ob_start();
-$command = '"'.$path_to_perl.'perl" '
-		  .'"'.$PATH_SQUT.'squt/parser/myparse_to_squt.pl" '
-		  .'"'.$query.'" '
-		  .($is_debug ? '"debug" ':'')
-		  .'2> '.$ERROR_OUTPUT_FILE;
-if ($is_debug) {
-	echo $command."\n\n";
-}
-
-if ($os == 'Windows') {
-	$WshShell = new COM("WScript.Shell");
-	$result = $WshShell->Exec($command)->StdOut->ReadAll;
+if (isset($_GET['sample'])) {
+	echo file_get_contents("querysamples/".preg_replace('#\.sql$#','_expected.json',$_GET['sample']));
 }
 else {
-	$result = shell_exec($command);
-}
-$error_output = file_get_contents($ERROR_OUTPUT_FILE);
-if (!empty($error_output)) {
-	echo json_encode(array('Error'=>file_get_contents($ERROR_OUTPUT_FILE)));
-}
-else {
-	echo $result;
+	$query = str_replace('"','\"',str_replace("\n"," ",$_GET['query']));
+	$is_debug = isset($_GET['debug']) && $_GET['debug'] == 1;
+	$path_to_perl = ($os == 'Windows' ? $PATH_TO_CYGWIN.'/bin/' : '');
+	
+	if (!file_exists($ERROR_OUTPUT_FILE)) {
+		echo 'Error - The file '.$ERROR_OUTPUT_FILE.' has not been found in the /front-end directory.';
+		exit(0);
+	}
+	ob_start();
+	$command = '"'.$path_to_perl.'perl" '
+			  .'"'.$PATH_SQUT.'squt/parser/myparse_to_squt.pl" '
+			  .'"'.$query.'" '
+			  .($is_debug ? '"debug" ':'')
+			  .'2> '.$ERROR_OUTPUT_FILE;
+	if ($is_debug) {
+		echo $command."\n\n";
+	}
+	
+	if ($os == 'Windows') {
+		$WshShell = new COM("WScript.Shell");
+		$result = $WshShell->Exec($command)->StdOut->ReadAll;
+	}
+	else {
+		$result = shell_exec($command);
+	}
+	$error_output = file_get_contents($ERROR_OUTPUT_FILE);
+	if (!empty($error_output)) {
+		echo json_encode(array('Error'=>file_get_contents($ERROR_OUTPUT_FILE)));
+	}
+	else {
+		echo $result;
+	}
 }
 
 ?>
