3185748dff9fe7bd9050b721eb8f5e921e75051e
Bruno Perel <brunoperel@gmail.com>
2013-04-04 20:00:21 +0200

    Prevent non-SELECT queries

--- parser/myparse_to_squt.pl
+++ parser/myparse_to_squt.pl
@@ -19,7 +19,10 @@ if ($debug) {
 	print Dumper $query;
 }
 
-if ($query->getCommand() eq "SQLCOM_ERROR") {
+if ($query->getCommand() ne "SQLCOM_SELECT") {
+	$sqlv_tables{"Error"}="Only SELECT queries are supported for now";
+}
+elsif ($query->getCommand() eq "SQLCOM_ERROR") {
 	$sqlv_tables{"Error"}=$query->getErrstr();
 }
 else {
