449363a2773686c639a811b344bc1529b487dcc8
Bruno Perel <brunoperel@gmail.com>
2013-05-29 23:52:49 +0200

    Rewrite the installation bash shell : getting rid of relative paths, stopping at errors and passing the MySQL source path to the Makefile maker

--- install_squt.sh
+++ install_squt.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 DIR=$PWD
+MYSQL_DL_DIR="/usr/src/";
+MYSQL_ARCHIVE_PREFIX="mysql-";
+MYSQL_VERSION="5.0.51a"
+MYSQL_ARCHIVE_NAME=$MYSQL_ARCHIVE_PREFIX$MYSQL_VERSION;
 
-cd ..
-wget http://downloads.mysql.com/archives/mysql-5.0/mysql-5.0.45.tar.gz
-tar xvzf mysql-5.0.45.tar.gz
-rm mysql-5.0.45.tar.gz
+cd $MYSQL_DL_DIR
+wget http://downloads.mysql.com/archives/mysql-5.0/$MYSQL_ARCHIVE_NAME.tar.gz
+tar xvzf $MYSQL_ARCHIVE_NAME.tar.gz
+rm $MYSQL_ARCHIVE_NAME.tar.gz
 
-cd mysql-5.0.45
-cat ../squt/parser/DBIx-MyParse-0.88/mysql.patch | patch -p1
+cd $MYSQL_ARCHIVE_NAME
+cat $DIR/parser/DBIx-MyParse-0.88/mysql.patch | patch -p1
 
 export CC="gcc -fPIC"
 export CXX="g++ -fPIC"
@@ -18,14 +22,20 @@ make install
 
 cd $DIR/parser/DBIx-MyParse-0.88/
 PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install JSON::PP'
-perl Makefile.PL
+perl Makefile.PL $MYSQL_DL_DIR$MYSQL_ARCHIVE_NAME
+OUT=$?
 
-make install
-mkdir /tmp/myparse
-mkdir /tmp/myparse/test
-make test
+if [ $OUT -eq 0 ];then
+	make install
+	OUT=$?
+	if [ $OUT -eq 0 ];then
+		mkdir /tmp/myparse
+		mkdir /tmp/myparse/test
+		make test
 
-export CC="gcc"
-export CXX="g++"
+		export CC="gcc"
+		export CXX="g++"
 
-chmod 666 $DIR/front-end/error_output.log
+		chmod 666 $DIR/front-end/error_output.log
+	fi
+fi
