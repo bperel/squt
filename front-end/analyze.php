7f4482150b3bedc95b785486d4813b758bc95169
Bruno Perel <brunoperel@gmail.com>
2013-06-02 13:06:38 +0200

    Config in an INI file

--- front-end/analyze.php
+++ front-end/analyze.php
@@ -1,7 +1,8 @@
 <?php
 header('Content-type: application/json');
-include_once('config.php');
 error_reporting(E_ALL);
+$conf = parse_ini_file("conf.ini");
+$error_output_file=$conf['error_output_file'];
 
 $os = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' ? 'Windows' : 'Linux';
 $is_win= $os == 'Windows';
@@ -12,23 +13,23 @@ if (isset($_POST['sample'])) {
 else {
 	$query = str_replace('"','\"',str_replace("\n"," ",str_replace('\\"','\\\"',$_POST['query'])));
 	$query = preg_replace('#[\s\t]+$#','',$query);
-	if (strlen($query) > $QUERY_MAX_LENGTH) {
+	if (strlen($query) > $conf['query_max_length']) {
 		echo json_encode(array('Error'=>'For performance and security reasons, squt does not allow queries longer than 2000 characters'));
 		exit(0);
 	}
 	$is_debug = isset($_POST['debug']) && $_POST['debug'] == 1;
-	$path_to_perl = ($os == 'Windows' ? $PATH_TO_CYGWIN.'/bin/' : '');
+	$path_to_perl = ($os == 'Windows' ? $conf['path_to_cygwin'].'/bin/' : '');
 	
-	if (!file_exists($ERROR_OUTPUT_FILE)) {
-		echo json_encode(array('Error'=>'The file '.$ERROR_OUTPUT_FILE.' has not been found in the /front-end directory.'));
+	if (!file_exists($error_output_file)) {
+		echo json_encode(array('Error'=>'The file '.$error_output_file.' has not been found in the /front-end directory.'));
 		exit(0);
 	}
 	ob_start();
 	$command = '"'.$path_to_perl.'perl" '
-			  .'"'.$PATH_SQUT.'squt/parser/myparse_to_squt.pl" '
+			  .'"'.$conf['path_to_squt'].'squt/parser/myparse_to_squt.pl" '
 			  .'"'.$query.'" '
 			  .($is_debug ? '"debug" ':'')
-			  .($is_win ? ('2> '.$ERROR_OUTPUT_FILE) : ('2>&1'));
+			  .($is_win ? ('2> '.$error_output_file) : ('2>&1'));
 	if ($is_debug) {
 		echo $command."\n\n";
 	}
@@ -36,13 +37,13 @@ else {
 	if ($is_win) {
 		$WshShell = new COM("WScript.Shell");
 		$result = $WshShell->Exec($command)->StdOut->ReadAll;
-		$error_output = file_get_contents($ERROR_OUTPUT_FILE);
+		$error_output = file_get_contents($error_output_file);
 	}
 	else {
 		$result = shell_exec($command);
 	}
 	if ($is_win && !empty($error_output)) {
-		echo json_encode(array('Error'=>file_get_contents($ERROR_OUTPUT_FILE)));
+		echo json_encode(array('Error'=>file_get_contents($error_output_file)));
 	}
 	else {
 		if (strpos($result,'{') === false) {
--- front-end/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$PATH_TO_CYGWIN="C:/cygwin"; // Ignore this line if you're not using Cygwin
-$PATH_SQUT="/usr/src/";
-$ERROR_OUTPUT_FILE="error_output.log";
-
-$QUERY_MAX_LENGTH = 2000;
-?>
