7115aa62a6df62f5c5c807ca41e3fa1f495c283f
Bruno Perel <brunoperel@gmail.com>
2014-08-20 14:15:00 +0200

    uglify and include-source Grunt tasks

--- front-end/Gruntfile.js
+++ front-end/Gruntfile.js
@@ -3,20 +3,42 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		includeSource: {
+			include: {
+				files: '<%= pkg.files.src %>'
+			},
+			target: {
+				files: {
+					'squt.html': 'squt.html'
+				}
+			}
+		},
 		uglify: {
 			build: {
 				files: [{
-					src: 'js/**/*.js',
-					dest: 'build/squt.js'
+					src: '<%= pkg.files.src %>',
+					dest: 'build/<%= pkg.name %>.js'
 				}]
+			},
+			options: {
+				banner: '/*! Generated <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n'
 			}
 		}
 	});
 
-	// Load the plugin that provides the "uglify" task.
+	grunt.config('env', grunt.option('env') || process.env.GRUNT_ENV || 'ci');
+
+
+	var tasks = [];
+	if(grunt.config('env') === 'ci') {
+		grunt.loadNpmTasks('grunt-include-source');
+		tasks.push('includeSource');
+	}
+
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	tasks.push('uglify');
 
 	// Default task(s).
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', tasks);
 
 };
