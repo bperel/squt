488828d517dd234d3991aa28db8fb3566b1bca68
Bruno Perel <brunoperel@gmail.com>
2014-09-29 23:29:24 +0200

    Add Apache alias creation to the install script

--- install_squt.sh
+++ install_squt.sh
@@ -5,12 +5,13 @@ usage(){
 	exit 1
 }
 
-DIR=$PWD
+SQUT_DIR=$PWD
 
 DEFAULT_MYSQL_PATH=true
 DO_PATCH=true
 DO_MYSQL_MAKE=true
 MYSQL_VERSION="5.0.96"
+APACHE_CONF_FILE="/etc/apache2/apache2.conf"
 
 while :
 do
@@ -53,6 +54,7 @@ do
     esac
 done
 
+# Download MySQL
 
 if $DEFAULT_MYSQL_PATH == true; then
 	MYSQL_DL_DIR="/usr/src/"
@@ -78,9 +80,11 @@ else
 	fi
 fi
 
+# Patch MySQL
+
 cd $MYSQL_FULL_DIR
 if $DO_PATCH == true; then
-	PATCH_FILE=$DIR/parser/DBIx-MyParse-0.88/patches/$MYSQL_ARCHIVE_NAME.patch
+	PATCH_FILE=$SQUT_DIR/parser/DBIx-MyParse-0.88/patches/$MYSQL_ARCHIVE_NAME.patch
 	if [ ! -f "$PATCH_FILE" ]; then
 		echo "FATAL : required patch file $PATCH_FILE doesn't exist !">&2
 		exit;
@@ -90,6 +94,8 @@ fi
 
 OLD_CC=$CC
 OLD_CXX=$CXX
+
+# Configure and compile MySQL
 
 if $DO_MYSQL_MAKE == true; then
 	export CC="gcc -fPIC"
@@ -113,7 +119,9 @@ if $DO_MYSQL_MAKE == true; then
 	fi
 fi
 
-cd $DIR/parser/DBIx-MyParse-0.88/
+# Generate the parser's Makefile
+
+cd $SQUT_DIR/parser/DBIx-MyParse-0.88/
 PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install JSON::PP'
 OUT=$?
 if [ $OUT -ne 0 ]; then
@@ -127,19 +135,48 @@ export CC="$OLD_CC"
 export CXX="$OLD_CXX"
 
 if [ $OUT -eq 0 ];then
+	# Compile the parser with the generated Makefile
 	make install
 	OUT=$?
 	if [ $OUT -eq 0 ];then
+		# Test the parser
 		mkdir /tmp/myparse
 		mkdir /tmp/myparse/test
 		make test
+		OUT=$?
+		if [ $OUT -eq 0 ];then
+			cd $SQUT_DIR/front-end
+			chmod 666 error_output.log
 
-		cd $DIR/front-end
-		chmod 666 error_output.log
-		
-		npm install
-		npm install -g grunt
-		npm install -g grunt-cli
-		grunt
+			npm install
+			npm install -g grunt
+			npm install -g grunt-cli
+			grunt
+			OUT=$?
+			if [ $OUT -eq 0 ];then
+				# Add an alias in the Apache config
+				if [ -e $APACHE_CONF_FILE ]; then
+					if grep -q squt $APACHE_CONF_FILE; then
+					  "It looks like squt already has an Apache alias, skipping the alias creation"
+					else
+						cat >> $APACHE_CONF_FILE <<-APACHE_ALIAS
+
+						Alias /squt "$SQUT_DIR"
+						<Directory "$SQUT_DIR/">
+							Options Indexes FollowSymLinks MultiViews
+							AllowOverride all
+							Order Allow,Deny
+							Allow from all
+						</Directory>
+						APACHE_ALIAS
+					fi
+				else
+					echo "The apache configuration $APACHE_CONF_FILE file does not exist or is not writeable, aborting the alias creation"
+				fi
+			fi
+		else
+			echo "The parser tests failed, aborting."
+			exit
+		fi
 	fi
 fi
